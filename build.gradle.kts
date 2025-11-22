30a3b088bb9ca6dde1a42a5d73f6f34d12f5ca81
ElCamacho05 <camachocastelanjm@gmail.com>
2025-11-21 22:52:13 -0600

    Cambio de Build Configuration Language de Kotlin a Graddle

--- build.gradle.kts
+++
@@ -1,8 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    id("com.android.library") version "7.4.2" apply false
-
-    // AÑADE ESTO: Declaramos que usaremos el plugin de Google Services en algún módulo
-    id("com.google.gms.google-services") version "4.4.0" apply false // Verifica tu versión
-}
